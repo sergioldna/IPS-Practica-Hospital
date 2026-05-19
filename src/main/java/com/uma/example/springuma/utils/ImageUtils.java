0e4afbc4c1d41a9b3ed702a2a8e117295f795b16
Sergio Aldana <sergioaldanagonzalez@gmail.com>
2026-05-19 11:32:48 +0200

    fix: ImageUtils - try-with-resources and IO handling

--- src/main/java/com/uma/example/springuma/utils/ImageUtils.java
+++ src/main/java/com/uma/example/springuma/utils/ImageUtils.java
@@ -1,44 +1,50 @@
 package com.uma.example.springuma.utils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
 public class ImageUtils {
 
-    public static byte[] compressImage(byte[] data) {
+    public static byte[] compressImage(byte[] data) throws IOException {
         Deflater deflater = new Deflater();
         deflater.setLevel(Deflater.BEST_COMPRESSION);
         deflater.setInput(data);
         deflater.finish();
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] tmp = new byte[4 * 1024];
-        while (!deflater.finished()) {
-            int size = deflater.deflate(tmp);
-            outputStream.write(tmp, 0, size);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+            byte[] tmp = new byte[4 * 1024];
+            while (!deflater.finished()) {
+                int size = deflater.deflate(tmp);
+                outputStream.write(tmp, 0, size);
+            }
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            deflater.end();
         }
-        try {
-            outputStream.close();
-        } catch (Exception ignored) {
-        }
-        return outputStream.toByteArray();
     }
 
-    public static byte[] decompressImage(byte[] data) {
+    public static byte[] decompressImage(byte[] data) throws IOException {
         Inflater inflater = new Inflater();
         inflater.setInput(data);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] tmp = new byte[4 * 1024];
-        try {
-            while (!inflater.finished()) {
-                int count = inflater.inflate(tmp);
-                outputStream.write(tmp, 0, count);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+            byte[] tmp = new byte[4 * 1024];
+            try {
+                while (!inflater.finished()) {
+                    int count = inflater.inflate(tmp);
+                    outputStream.write(tmp, 0, count);
+                }
+            } catch (DataFormatException e) {
+                throw new IOException("Failed to decompress image data", e);
             }
-            outputStream.close();
-        } catch (Exception ignored) {
+            return outputStream.toByteArray();
+        } finally {
+            inflater.end();
         }
-        return outputStream.toByteArray();
     }
 
 }
